c9868f805fb6bd4ba5cf2342f82511af93fafed0
ahmadbagwi <ahmad.kdesain@gmail.com>
2018-09-07 16:21:53 +0700

    pengiriman email dengan wp_mail() didukung plugin easy smtp

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Plugin Name: WP Email Blast Multisite
+* Plugin Name: WP Email Blast
 * Plugin URI: http://kdesain.com
 * Description: Mengirim artikel harian yang ditulis oleh setiap situs multisite setiap hari
 * Version: 0.1
@@ -10,19 +10,110 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 date_default_timezone_set("Asia/Jakarta");
 function wpemailblast_actions() {
-     add_menu_page('WP Email Blast', 'WP Email Blast', 'edit_pages', 'wp_emailblast', 'wpemailblast', '', 86);
+     add_menu_page('WP Email Blast', 'WP Email Blast', 'edit_pages', 'wp_emailblast', 'wpemailblast', '', 76);
      add_submenu_page('wp_emailblast', 'Tambah Email', 'Tambah Email', 'edit_pages', 'tambah_email', 'tambahemail' );
      add_submenu_page('wp_emailblast', 'Import CSV', 'Import CSV', 'edit_pages', 'import_csv', 'importcsv' );
      add_submenu_page('wp_emailblast', 'Hapus Email', 'Hapus Email', 'edit_pages', 'hapus_email', 'hapusemail' );
 }
 add_action('admin_menu', 'wpemailblast_actions');
 
-function bootstrap_stylesheet() {
-    wp_enqueue_style( 'bootstrapCSS', plugins_url( '/style/bootstrap.css', __FILE__ ) );
+register_activation_hook(__FILE__, 'my_activation');
+
+function my_activation() {
+    if (! wp_next_scheduled ( 'email_kebudayaan' )) {
+	wp_schedule_event( strtotime('06:05:00'), 'daily', 'email_kebudayaan' );
+    }
 }
 
-if(($_GET['page'] == 'wp_emailblast') or ($_GET['page'] == 'tambah_email') or ($_GET['page'] == 'import_csv') or ($_GET['page'] == 'hapus_email')){
-	add_action('admin_print_styles', 'bootstrap_stylesheet');
+add_action('email_kebudayaan', 'kirimemail');
+
+function kirimemail() {
+	$blogs = get_last_updated(' ', 0, 40);
+	global $wpdb;
+	foreach ($blogs AS $blog) {    
+		switch_to_blog($blog["blog_id"]);
+		$today = getdate();
+		$args = array(
+			'post_type'         => 'post',
+			'post_status'       => 'publish',
+			'date_query'        => array(
+				array(
+					'year'  => $today['year'],
+					'month' => $today['mon'],
+					'day'   => $today['mday']
+				)
+			)
+		);
+		$wpb_all_query = new WP_Query($args); ?>
+		<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+			<!-- the loop -->
+			<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); 
+				$konten = get_the_date('Y-m-d')." | ".get_bloginfo()." | ".'<a href="'.get_permalink().'">'.get_the_title()."</a><br>";
+				$semuakonten[]=$konten
+				?>
+			<?php endwhile; ?>
+			<!-- end of the loop -->
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
+		<?php restore_current_blog(); ?>
+	<?php 
+	}
+
+	$artikel = implode(" ",$semuakonten);
+
+	//footer
+	$footer = '<br>Ikuti akun @budayasaya di Facebook, Twitter dan Instagram
+	<br>Untuk berhenti menerima email klik 
+	<a href="https://kebudayaan.kemdikbud.go.id/stop-email-kebudayaan/">disini</a>';
+	if ($artikel != NULL) {
+	// mengambil penerima
+		$ambilemail = $wpdb->get_results( "SELECT email FROM penerima WHERE status='1'");
+		foreach ($ambilemail as $listemail) {
+			$hasilemail = $listemail->email;
+			$dataemail[] = $hasilemail;
+		}
+		$subject = 'Berita Kebudayaan';
+		$headers = array('Content-Type: text/html; charset=UTF-8');
+		//kirim email dengan wp_mail() format html
+		foreach ($dataemail as $to) {
+			wp_mail( $to, $subject, $artikel, $headers ); 
+		}
+		/*fungsi kirim email dengan phpmailer
+		require ('vendor/phpmailer/phpmailer/PHPMailerAutoload.php');
+		require ('vendor/phpmailer/phpmailer/class.phpmailer.php');
+		require ('vendor/phpmailer/phpmailer/class.smtp.php');
+
+    	// Konfigurasi SMTP
+		$mail = new PHPMailer;
+		$mail->isSMTP();
+		$mail->SMTPKeepAlive = true;
+		$mail->SmtpConnect();
+		$mail->Host = 'mail.smtp2go.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'budayasaya@gmail.com';
+		$mail->Password = 'SahabatBudaya';
+		$mail->SMTPSecure = 'tls';
+		$mail->Port = 2525;
+		$mail_class = "transactional";
+		$mail->setFrom('budayasaya@gmail.com', 'budayasaya');
+		$mail->addReplyTo('budayasaya@gmail.com', 'budayasaya');
+		$mail->Subject = 'Berita Kebudayaan';
+		$mail->addCustomHeader("X-GreenArrow-MailClass: $mail_class");
+
+    // looping pengiriman email
+		foreach ($dataemail as $penerima) {
+			$mail->ClearAllRecipients();
+			$mail->AddAddress($penerima);
+			$mail->isHTML(true);
+			$mail->Body = $artikel.$footer;
+			if(!$mail->send()){
+				echo 'Pesan tidak dapat dikirim.';
+				echo 'Mailer Error: ' . $mail->ErrorInfo;
+			}
+		}
+		$mail->SmtpClose();*/
+	}
 }
 
 function wpemailblast() { 
@@ -108,100 +199,6 @@ function hapusemail() { ?>
 }
 
 add_shortcode('unsubscribe', 'hapusemail');
-
-// Fungsi cron dan kirim email
-register_activation_hook(__FILE__, 'my_activation');
-
-function my_activation() {
-    if (! wp_next_scheduled ( 'email_kebudayaan' )) {
-	wp_schedule_event( strtotime('06:05:00'), 'daily', 'email_kebudayaan' );
-    }
-}
-
-add_action('email_kebudayaan', 'cronkirimemail');
-
-function cronkirimemail() {
-	global $wpdb;
-	$blogs = get_last_updated();
-	foreach ($blogs AS $blog) {    
-		switch_to_blog($blog["blog_id"]);
-		$today = getdate();
-		$args = array(
-			'post_type'         => 'post',
-			'post_status'       => 'publish',
-			'date_query'        => array(
-				array(
-					'year'  => $today['year'],
-					'month' => $today['mon'],
-					'day'   => $today['mday']
-				)
-			)
-		);
-		$wpb_all_query = new WP_Query($args); ?>
-		<?php if ( $wpb_all_query->have_posts() ) : ?>
-
-			<!-- the loop -->
-			<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); 
-				$konten = get_the_date('Y-m-d')." | ".get_bloginfo()." | ".'<a href="'.get_permalink().'">'.get_the_title()."</a><br>";
-				$semuakonten[]=$konten
-				?>
-			<?php endwhile; ?>
-			<!-- end of the loop -->
-			<?php wp_reset_postdata(); ?>
-		<?php endif; ?>
-		<?php restore_current_blog(); 
-	}
-
-	$artikel = implode(" ",$semuakonten);
-
-//footer
-	$footer = '<br>Ikuti akun @budayasaya di Facebook, Twitter dan Instagram
-	<br>Untuk berhenti menerima email klik 
-	<a href="https://kebudayaan.kemdikbud.go.id/stop-email-kebudayaan/">disini</a>';
-if ($artikel != NULL) {
-// mengambil penerima
-	$ambilemail = $wpdb->get_results( "SELECT email FROM penerima WHERE status='1'");
-	foreach ($ambilemail as $listemail) {
-		$hasilemail = $listemail->email;
-		$dataemail[] = $hasilemail;
-	} 
-
-//fungsi kirim email dengan phpmailer
-	require ('vendor/phpmailer/phpmailer/PHPMailerAutoload.php');
-	require ('vendor/phpmailer/phpmailer/class.phpmailer.php');
-	require ('vendor/phpmailer/phpmailer/class.smtp.php');
-
-    // Konfigurasi SMTP
-	$mail = new PHPMailer;
-	$mail->isSMTP();
-	$mail->SMTPKeepAlive = true;
-	$mail->SmtpConnect();
-	$mail->Host = 'mail.smtp2go.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = 'budayasaya@gmail.com';
-	$mail->Password = 'SahabatBudaya';
-	$mail->SMTPSecure = 'tls';
-	$mail->Port = 2525;
-	$mail_class = "transactional";
-	$mail->setFrom('budayasaya@gmail.com', 'budayasaya');
-	$mail->addReplyTo('budayasaya@gmail.com', 'budayasaya');
-	$mail->Subject = 'Berita Kebudayaan';
-	$mail->addCustomHeader("X-GreenArrow-MailClass: $mail_class");
-
-    // looping pengiriman email
-	foreach ($dataemail as $penerima) {
-		$mail->ClearAllRecipients();
-		$mail->AddAddress($penerima);
-		$mail->isHTML(true);
-		$mail->Body = $artikel.$footer;
-		if(!$mail->send()){
-			echo 'Pesan tidak dapat dikirim.';
-			echo 'Mailer Error: ' . $mail->ErrorInfo;
-		}
-	}
-	$mail->SmtpClose();
-}
-}
 
 register_deactivation_hook(__FILE__, 'my_deactivation');
 
